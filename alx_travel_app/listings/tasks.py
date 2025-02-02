4c3499d4a849bfbfdfea8ae84e750641e956729d
8srael <kouassiisrael36@gmail.com>
2025-02-02 16:17:05 +0000

    email booking updated🆗

--- alx_travel_app/listings/tasks.py
+++ alx_travel_app/listings/tasks.py
@@ -1,12 +1,26 @@
 from celery import shared_task
 from django.core.mail import send_mail
-from alx_travel_app.settings import EMAIL_HOST_USER
-
+from django.conf import settings
 
 @shared_task
 def send_booking_confirmation_email(email, booking_details):
-    subject = 'Booking Confirmation'
-    message = f'Hey,\n\nBooking done sucessfuly. booking details : {booking_details}'
+    subject = 'Your Booking Confirmation 🚀'
 
-    send_mail(subject, message, EMAIL_HOST_USER, [email])
-    return 'Mail sent successfully'
+    message = f"""
+    Hello {booking_details.get('user').last_name} {booking_details.get('user').first_name}, ,
+
+    We're excited to confirm your booking! 🎉
+
+    📋 **Booking Details:**  
+    Listing: {booking_details.get('listing')}  
+    ✅ Status: Confirmed  
+    📅 Dates: From {booking_details.get('start_date')} to {booking_details.get('end_date')}  
+    💰 Total Price: {booking_details.get('total_price')} €
+
+    If you have any questions, feel free to reach out to us.
+
+    Thank you for choosing ALX Travel App! 🌍
+    """
+
+    send_mail(subject, message, settings.EMAIL_HOST_USER, [email])
+    return 'Confirmation email sent successfully ✅'
